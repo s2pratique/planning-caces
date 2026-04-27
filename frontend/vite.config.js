bab1706a75c7a51a5b6cb80c839799570bcba7b6
s2pratique <s2pratique@gmail.com>
2026-04-27 18:03:44 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    open: true
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://planning-caces-production.up.railway.app',
+        changeOrigin: true,
+      }
+    }
   }
 })
